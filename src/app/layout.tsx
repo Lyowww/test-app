f75a1cb2f7970c551c9bcb7a3ff59d712a7e3590
Lyowww <lyova.hovhannisyan76@gmail.com>
2025-10-28 19:26:36 +0400

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         {children}
         <script
           dangerouslySetInnerHTML={{
-            __html: `<script src="https://charbot-mobi.vercel.app/chatbot-widget.js" chat_url="https://moby.host/app/hAW9B"></script>`,
+            __html: `<script src="https://charbot-mobi.vercel.app/chatbot-widget.js" chat_url="https://moby.host/app/hAW9B"/>`,
           }}
         >
           </script>
